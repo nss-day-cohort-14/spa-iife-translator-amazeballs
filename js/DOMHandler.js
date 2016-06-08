0523bdff46709d4103321365fe9c356b0015779f
Dan Day <danday@gmail.com>
2016-06-08 14:41:54 -0500

    working on DOMHandler

---
+++ js/DOMHandler.js
@@ -0,0 +1,35 @@
+
+
+
+function translator () {
+  outputArea.innerHTML += "<p>Test</p>"
+  console.log("userInput:", userInput.value);
+
+  console.log("button clicked:", event.target.id);
+
+  languageTranslator[event.target.id]
+
+}
+
+
+
+
+
+//get button elements by id
+var userInput = document.getElementById("userInput")
+
+var outputArea = document.getElementById("outputArea")
+
+var germanButton = document.getElementById("german")
+
+var frenchButton = document.getElementById("french")
+
+var japaneseButton = document.getElementById("japanese")
+
+
+// add event listeners
+germanButton.addEventListener("click", translator);
+
+frenchButton.addEventListener("click", translator);
+
+japaneseButton.addEventListener("click", translator);
---
+++ js/languageTranslator.js
@@ -0,0 +1,20 @@
+var languageTranslator = (function () {
+
+//private variables
+
+var translatedText = null;
+
+return {
+
+  getTranslatedText: function () {
+    return translatedText;
+  }
+
+  
+  
+}
+
+
+
+
+})();
