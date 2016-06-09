eb070c168638f50079cc1e980c395c0c53ad74ef
Dan Day <danday@gmail.com>
2016-06-09 10:30:33 -0500

    speaknspell

--- js/languageTranslator.js
+++ js/languageTranslator.js
@@ -18,7 +18,12 @@ return {
   reassembleOutput: function() {
     // console.log(translatedText.join(" "));
     outputArea.innerHTML = "";
-    outputArea.innerHTML = translatedText.join(" ");
+    var finalText = translatedText.join(" ");
+    outputArea.innerHTML = finalText;
+
+    var utterance = new SpeechSynthesisUtterance(finalText);
+    window.speechSynthesis.speak(utterance);
+
     translatedText = [];
   }
 }
